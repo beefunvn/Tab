b4d3354ada668740d45354598426c7c1815418ef
Trần Đức Minh <147921062+beefunvn@users.noreply.github.com>
2024-06-26 03:03:53 +0700

    Update Tab.java

--- src/main/java/dev/tranducminh/tab/Tab.java
+++ src/main/java/dev/tranducminh/tab/Tab.java
@@ -5,8 +5,7 @@ import dev.tranducminh.tab.Manager.TabManager;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
+import java.util.concurrent.TimeUnit;
 
 public class Tab extends JavaPlugin {
     private TabManager tabManager;
@@ -16,12 +15,11 @@ public class Tab extends JavaPlugin {
         saveDefaultConfig();
         this.tabManager = new TabManager(this);
         getServer().getPluginManager().registerEvents((Listener)new PlayerJoinListener(this.tabManager), (Plugin)this);
-        BukkitTask task = (new BukkitRunnable() {
-            public void run() {
-                Tab.this.tabManager.updateTabListForAllPlayers();
-            }
-        }).runTaskTimer((Plugin)this, 0L, 20L);
-    }
+
+        getServer().getAsyncScheduler().runAtFixedRate(this, task -> {
+            this.tabManager.updateTabListForAllPlayers();
+    }, 1L, 50L, TimeUnit.MILLISECONDS);
+}
 
     public TabManager getTabManager() {
         return this.tabManager;
